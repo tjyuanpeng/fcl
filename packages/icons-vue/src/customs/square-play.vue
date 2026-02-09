2a6256c3990cd7288b8dce537f4db5a9fdc7d6b4
liulifu <liulifu@falconix.net>
2026-02-09 15:01:55 +0800

    feat: add new icon "square-play" to icons-vue

---
+++ packages/icons-vue/src/customs/square-play.vue
@@ -0,0 +1,6 @@
+<template>
+  <svg viewBox="0 0 18 18" xmlns="http://www.w3.org/2000/svg">
+    <rect x="1.5" y="2.5" width="15" height="13" rx="5.5" stroke="currentColor" />
+    <path d="M10.5 8.13397C11.1667 8.51887 11.1667 9.48113 10.5 9.86603L9 10.7321C8.33333 11.117 7.5 10.6358 7.5 9.86603L7.5 8.13397C7.5 7.36417 8.33333 6.88305 9 7.26795L10.5 8.13397Z" fill="currentColor" />
+  </svg>
+</template>
