1eec9deead321cd11ca6b41335e0c337e1eb27e1
tjyuanpeng <tjyuanpeng@gmail.com>
2026-04-03 13:33:31 +0800

    feat: micro-app v1.1.0

---
+++ packages/micro-app/src/hooks/micro-app.ts
@@ -0,0 +1,52 @@
+export * from './event'
+
+export const isInMicroapp = (): boolean => Boolean(window.__POWERED_BY_WUJIE__)
+
+export const isNewFramework = (): boolean => Boolean(window.$wujie?.props?.newFramework)
+
+export const isFrameMode = (): boolean => window.location.pathname.startsWith('/_web')
+
+export const isClient = (): boolean => false
+
+export const getClientInfo = (): unknown => ({})
+
+export const getToken = (): string | null => localStorage.getItem('token')
+
+export const getButtonPermissions = (): string | null => localStorage.getItem('buttonPermissions')
+
+export const getMenuPermissions = (): string | null => localStorage.getItem('menuPermissions')
+
+export const getCurrentEntityId = (): string | null => localStorage.getItem('CURR_ENTITY_ID')
+
+export const pushMain = (fullPath: string): void => {
+  window.top?.history.pushState(window.history.state, '', fullPath)
+  window.top?.dispatchEvent(new PopStateEvent('popstate', { state: window.top.history.state }))
+}
+
+export const replaceMain = (fullPath: string): void => {
+  window.top?.history.replaceState(window.history.state, '', fullPath)
+  window.top?.dispatchEvent(new PopStateEvent('popstate', { state: window.top.history.state }))
+}
+
+export const replacePathname = (url: string, from: string, to: string): string => {
+  const u = new URL(url, location.origin)
+  u.pathname = u.pathname.replace(from, to)
+  return u.toString()
+}
+
+export const openNewWindow = (url: string, { target = '_blank', frameMode = false }: { target?: string, frameMode?: boolean } = {}): void => {
+  const u = new URL(url, location.origin)
+  url = u.toString()
+  if (frameMode && u.origin === location.origin) {
+    url = replacePathname(url, '/web', '/_web')
+  }
+  window.top?.open(url, target)
+}
+
+export const setHrefMain = (url: string): void => {
+  window.top && (window.top.location.href = url)
+}
+
+export const getUserInfo = (): any => window.$wujie?.props?.getUserInfo()
+
+export const refreshRedDot = (): void => window.$wujie?.props?.refreshRedDot()
--- packages/micro-app/src/hooks/use-micro-app-props.ts
+++ packages/micro-app/src/hooks/use-micro-app-props.ts
@@ -1,18 +1,17 @@
 import type { Ref } from 'vue'
 import { readonly, ref, watch } from 'vue'
-import { emitEvent } from './emit-event'
-import { onEvent } from './on-event'
+import { $emit, $on } from './event'
 
 export const useMicroAppProps = (defaultValue?: Partial<$WujieXProps>, { readonly: isReadOnly = true } = {}): Ref<$WujieXProps> => {
   const props = ref<$WujieXProps>(Object.assign({}, window.$wujie?.props, defaultValue))
 
   if (window.__WUJIE) {
-    onEvent(`@wujie-x/props-change:${(window.__WUJIE as any).id}` as 'WujieXCustomEvent', () => {
-      props.value = window.$wujie?.props as any
+    $on(`@wujie-x/props-change:${(window.__WUJIE as any).id}` as '@wujie-x/props-change', () => {
+      props.value = Object.assign({}, props.value, window.$wujie?.props)
     })
     if (!isReadOnly) {
       watch(props, () => {
-        emitEvent(`@wujie-x-vue/update-props:${(window.__WUJIE as any).id}` as 'WujieXCustomEvent', props.value)
+        $emit(`@wujie-x-vue/update-props:${(window.__WUJIE as any).id}` as '@wujie-x-vue/update-props', props.value)
       }, { deep: true })
     }
   }
